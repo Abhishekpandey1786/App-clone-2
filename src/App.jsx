d9a3c859729cfce3b330b0922fd1b45030520c08
Abhishek Pandey <your_abhishekpandey3119@gmail.com>
2025-05-06 15:24:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from './assets/Navbar'
+import './App.css'
+import Banner from './assets/Banner'
+import Poster from './assets/Poster'
+import Popular from './assets/Popular'
+import Footer from './assets/Footer'
+import About from './assets/About'
+function App() {
+
+
+  return (
+    <>
+      <Navbar/>
+      <Banner/>
+      <Poster/>
+      <Popular/>
+      <About/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
